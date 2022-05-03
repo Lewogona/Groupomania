254d792050cac28939cb1cb50cf5868583bc3595
Lewogona <lewogona@gmail.com>
2022-05-03 22:16:43 +0200

    git init

---
+++ front/src/components/LoginPage.vue
@@ -0,0 +1,23 @@
+<template> 
+    <div>
+        <img alt="Vue logo" src="../assets/logo.png">
+        <div>
+            <b-button>Button</b-button>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: 'LoginPage',
+  props: {
+    
+  }
+}
+
+</script>
+
+<style scoped lang="scss">
+
+</style>
