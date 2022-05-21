0a1ec8b62bd3eaace3ce51ddbba139dcd922a4bd
Lewogona <lewogona@gmail.com>
2022-05-21 16:43:25 +0200

    cleaned

--- front/src/components/UsersInfo.vue
+++ front/src/components/UsersInfo.vue
@@ -3,19 +3,19 @@
         <b-col cols="9" align-self="center">
             <b-card class="mb-3" >
                 <b-row align-h="around" align-v="center">
-                    <b-col sm="3" lg="3" class="my-1">
+                    <b-col md="3" lg="3" class="my-1">
                         <b-card-text><strong>Prénom : </strong></b-card-text>
                         <b-card-text>{{ firstName }} </b-card-text>
                     </b-col>
-                    <b-col sm="3" lg="3" class="my-1">
+                    <b-col md="3" lg="3" class="my-1">
                         <b-card-text><strong>Nom : </strong></b-card-text>
                         <b-card-text>{{ lastName }}</b-card-text>
                     </b-col>
-                    <b-col sm="4" lg="5" class="my-1">
+                    <b-col md="4" lg="4" class="my-1">
                         <b-card-text><strong>Email : </strong></b-card-text>
                         <b-card-text>{{ email }}</b-card-text>
                     </b-col>
-                    <b-col sm="2" lg="1" class="my-1">
+                    <b-col md="2" lg="2" class="my-1">
                         <b-button 
                             @click="displayDeleteMessage(userId)"
                             variant="outline-danger">
