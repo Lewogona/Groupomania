9029329967b708869c6cc905d3a97b18eaf702a0
Lewogona <lewogona@gmail.com>
2022-05-23 11:44:59 +0200

    added hour and minute to date

--- front/src/services/date-service.js
+++ front/src/services/date-service.js
@@ -3,9 +3,13 @@ exports.getReadableDate = (ISOStringDate) => {
     const year = date.getFullYear();
     let month = date.getMonth()+1;
     let dt = date.getDate();
+    let hour = date.getHours();
+    let minute = date.getMinutes();
 
     if (dt < 10) dt = '0' + dt;
     if (month < 10) month = '0' + month;
+    if (minute < 10) minute = '0' + minute;
+    if (hour < 10) hour = '0' + hour;
 
-    return `${dt}/${month}/${year}`
+    return `${dt}/${month}/${year} à ${hour}:${minute}`
 }
